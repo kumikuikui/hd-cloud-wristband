fe4e79e0995f8386a123ed20b270e37f6c286b05
yaojie <yaojie@hadoop-tech.com>
2018-12-29 14:22:37 +0800

    手环

---
+++ src/main/java/com/coins/cloud/WristbandServiceApplication.java
@@ -0,0 +1,66 @@
+package com.coins.cloud;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+/**
+ * 
+  * @ClassName: WristbandServiceApplication
+  * @Description: 医疗手环
+  * @author yaojie yao.jie@hadoop-tech.com
+  * @Company hadoop-tech
+  * @date 2018年12月29日 下午2:19:59
+  *
+ */
+@EnableScheduling
+@SpringBootApplication
+public class WristbandServiceApplication extends WebMvcConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WristbandServiceApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/templates/**")
+				.addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/templates/");
+		registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
+		super.addResourceHandlers(registry);
+	}
+
+	@Bean
+	public HttpMessageConverters faHttpMessageConverters() {
+		FastJsonHttpMessageConverter faConverter = new FastJsonHttpMessageConverter();
+		List<MediaType> supportedMediaTypes = new ArrayList<>();
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON);
+		supportedMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+		supportedMediaTypes.add(MediaType.APPLICATION_ATOM_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_FORM_URLENCODED);
+		supportedMediaTypes.add(MediaType.APPLICATION_OCTET_STREAM);
+		supportedMediaTypes.add(MediaType.APPLICATION_PDF);
+		supportedMediaTypes.add(MediaType.APPLICATION_RSS_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XHTML_XML);
+		supportedMediaTypes.add(MediaType.APPLICATION_XML);
+		supportedMediaTypes.add(MediaType.IMAGE_GIF);
+		supportedMediaTypes.add(MediaType.IMAGE_JPEG);
+		supportedMediaTypes.add(MediaType.IMAGE_PNG);
+		supportedMediaTypes.add(MediaType.TEXT_EVENT_STREAM);
+		supportedMediaTypes.add(MediaType.TEXT_HTML);
+		supportedMediaTypes.add(MediaType.TEXT_MARKDOWN);
+		supportedMediaTypes.add(MediaType.TEXT_PLAIN);
+		supportedMediaTypes.add(MediaType.TEXT_XML);
+		faConverter.setSupportedMediaTypes(supportedMediaTypes);
+		return new HttpMessageConverters(faConverter);
+	}
+}
---
+++ src/main/java/com/coins/cloud/util/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.coins.cloud.util;
+
+/**
+ * 
+ * @ClassName: ErrorCode
+ * @Description: 错误码
+ * @author ShengHao shenghaohao@hadoop-tech.com
+ * @Company hadoop-tech
+ * @date 2018年3月15日 上午9:38:35
+ *
+ */
+public class ErrorCode {
+
+	/**
+	 * 成功
+	 */
+	public static final String SUCCESS = "1000000";
+
+	/**
+	 * 失败
+	 */
+	public final static String FAIL = "1000001";
+
+}
